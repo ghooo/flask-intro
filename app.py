ace4e2ec266fac3ffa2894dee0ed94e9a99986e5
ghooo <ghooo.ghooo@gmail.com>
2015-09-16 13:32:28 +0300

    home, welcome and login pages

---
+++ app.py
@@ -0,0 +1,28 @@
+# import the Flask class from the flask module
+from flask import Flask, render_template, redirect, url_for, request
+
+# create the application object
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+	return "Hello, World!"
+
+@app.route('/welcome')
+def welcome():
+	return render_template("welcome.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or \
+				request.form['password'] != 'admin':
+			error = 'Invalid credentials. Please retry again.'
+		else:
+			return redirect(url_for('home'))		
+	return render_template('login.html', error=error)
+
+if __name__ == '__main__':
+	app.run(debug=True)
