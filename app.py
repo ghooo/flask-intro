e98a536657ea27ed1ee062a58bd2aa39edc06ef8
ghooo <ghooo.ghooo@gmail.com>
2015-09-16 13:57:17 +0300

    3. session, logout, login_required

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, \
+		flash
+from functools import wraps
 
 # create the application object
 app = Flask(__name__)
 
+app.secret_key = "no no no no never"
+
+# login required decorator
+def login_required(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			flash('You need to login first')
+			return redirect(url_for('login'))
+	return wrap
 
 @app.route('/')
+@login_required
 def home():
-	return "Hello, World!"
+	return render_template("index.html")
 
 @app.route('/welcome')
 def welcome():
@@ -21,8 +36,17 @@ def login():
 				request.form['password'] != 'admin':
 			error = 'Invalid credentials. Please retry again.'
 		else:
+			session['logged_in'] = True
+			flash('You were just logged in!')
 			return redirect(url_for('home'))		
 	return render_template('login.html', error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+	session.pop('logged_in', None)
+	flash('You just logged out!')
+	return redirect(url_for('welcome'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
