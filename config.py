aaa1b017b086832a79ebdb2426fd35fb525e3910
ghooo <ghooo.ghooo@gmail.com>
2015-09-19 21:40:48 +0300

    11. secret key

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 #default config
 class BaseConfig(object):
 	DEBUG = False
-	SECRET_KEY = "you bet"
+	SECRET_KEY = "\x92!\x06\x89{d\xb4P\x80\xd4\xe5\xb0\x91" \
+			"\x17\xa8\xdd\xc1\x15U`H\xf0\x11z"
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(BaseConfig):
