4ebe4eb6acf8fbce5be186c1ba92e0999793269a
ghooo <ghooo.ghooo@gmail.com>
2015-09-21 14:29:22 +0300

    14. Local Postgresql setup
    a) setup postgres locally
    b) change the env variable locally (maybe use autoenv)
    c) setup our database locally - db_create.py

--- db_create.py
+++ db_create.py
@@ -7,6 +7,7 @@ db.create_all()
 # insert
 db.session.add(BlogPost("Good", "I\'m good."));
 db.session.add(BlogPost("Well", "I\'m well."));
+db.session.add(BlogPost("postgres", "we setup a local postgres"))
 
 # commit the changes
 db.session.commit()
