7051db6f7caf7389ae61badfa7cf4ecbfdd7e404
ghooo <ghooo.ghooo@gmail.com>
2015-09-21 17:42:11 +0300

    15. Migration with Flask Migrate
    a) python manage.py db init
    b) python manage.py db migrate
    c) python manage.py db upgrade

--- db_create.py
+++ db_create.py
@@ -1,13 +1,16 @@
 from app import db
-from models import BlogPost
+from models import BlogPost, User
 
 #create the database and the db tables
-db.create_all()
+# db.create_all()
 
 # insert
-db.session.add(BlogPost("Good", "I\'m good."));
-db.session.add(BlogPost("Well", "I\'m well."));
-db.session.add(BlogPost("postgres", "we setup a local postgres"))
+db.session.add(User("admin", "ad@min.com", "admin"))
+db.session.add(User("ghooo", "ghooo@ghoo.com", "ghooo"))
+
+db.session.add(BlogPost("Good", "I\'m good.", 1));
+db.session.add(BlogPost("Well", "I\'m well.", 1));
+db.session.add(BlogPost("postgres", "we setup a local postgres", 2))
 
 # commit the changes
 db.session.commit()
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from app import app, db
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 class BlogPost(db.Model):
 
@@ -7,10 +9,30 @@ class BlogPost(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String, nullable=False)
 	description = db.Column(db.String, nullable=False)
-
-	def __init__(self, title, description):
+	author_id = db.Column(db.Integer, ForeignKey('users.id'))
+	
+	def __init__(self, title, description, author_id):
 		self.title = title
 		self.description = description
+		self.author_id = author_id
 
 	def __repr__(self):
 		return '<title {}'.format(self.title)
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    posts = relationship("BlogPost", backref="author")
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<name - {}>'.format(self.name)
