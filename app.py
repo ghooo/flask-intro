415031c8524548b8b01e3f7d0efc683b908cf07f
ghooo <ghooo.ghooo@gmail.com>
2015-09-21 11:44:23 +0300

    updated config

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 # config
 import os
 app.config.from_object(os.environ['APP_SETTINGS'])
-
+print "PRINTING WORKS"
+print os.environ['APP_SETTINGS']
 
 
 # create the sqlalchemy object
